6c1a9f57d925cd3d3c779532c0e286ae5595268d
Gerd Wütherich <gerd.wuetherich@code-kontor.io>
2026-05-14 18:16:57 +0200

    wip

---
+++ org.slizaa.core.mvnresolver.api/src/main/java/org/slizaa/core/mvnresolver/api/IMvnCoordinate.java
@@ -0,0 +1,16 @@
+package org.slizaa.core.mvnresolver.api;
+
+public interface IMvnCoordinate {
+
+  String getGroupId();
+
+  String getArtifactId();
+
+  String getPackagingType();
+
+  String getClassifier();
+
+  String getVersion();
+  
+  String toCanonicalForm();
+}
