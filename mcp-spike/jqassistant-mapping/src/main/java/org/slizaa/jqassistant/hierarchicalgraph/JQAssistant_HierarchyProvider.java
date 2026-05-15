52b121a4dcd607bd109b3e5d81746eef3bb5fe69
Gerd Wütherich <gerd.wuetherich@code-kontor.io>
2026-05-15 23:31:34 +0200

    Improved hierarchy provider

--- mcp-spike/jqassistant-mapping/src/main/java/org/slizaa/jqassistant/hierarchicalgraph/JQAssistant_HierarchyProvider.java
+++ mcp-spike/jqassistant-mapping/src/main/java/org/slizaa/jqassistant/hierarchicalgraph/JQAssistant_HierarchyProvider.java
@@ -7,7 +7,10 @@ public class JQAssistant_HierarchyProvider extends AbstractQueryBasedHierarchyPr
 	@Override
 	protected String[] toplevelNodeIdQueries() {
 		return new String[] {
-				"MATCH (a:Artifact:Jar) RETURN id(a) as id"
+				// scanned jars directly
+				"MATCH (a:Artifact:Jar) RETURN id(a) as id",
+				// scanned maven projects
+				"MATCH (a:Project:File:Maven:Directory)-[CREATES]->(b:Artifact:Maven:File) WHERE a.packaging = 'jar' AND (b:Main OR b:Test) RETURN id(a) as id"
 		};
 	}
 
@@ -15,14 +18,17 @@ public class JQAssistant_HierarchyProvider extends AbstractQueryBasedHierarchyPr
 	protected String[] parentChildNodeIdsQueries() {
 		return new String[] {
 				// Artifact -> top-level Packages
-				"MATCH (a:Artifact:Jar)-[:CONTAINS]->(p:Package) WHERE NOT (:Package)-[:CONTAINS]->(p) RETURN id(a), id(p)",
+				"MATCH (a:Artifact:Jar)-[:CONTAINS]->(b:Package) WHERE NOT (:Package)-[:CONTAINS]->(b) RETURN id(a), id(b)",
+				"MATCH (a:Project:File:Maven:Directory)-[CREATES]->(b:Artifact:Maven:File) WHERE a.packaging = 'jar' AND (b:Main OR b:Test) RETURN id(a), id(b)",
+				"MATCH (a:Artifact:Maven:File:Main)-[:CONTAINS]->(b:Package) where a.type = 'jar' AND NOT (:Package)-[:CONTAINS]->(b) RETURN id(a), id(b)",
+				"MATCH (a:Artifact:Maven:File:Test)-[:CONTAINS]->(b:Package) where a.type = 'test-jar' AND NOT (:Package)-[:CONTAINS]->(b) RETURN id(a), id(b)",
 				// Package -> sub-Packages
-				"MATCH (p1:Package)-[:CONTAINS]->(p2:Package) RETURN id(p1), id(p2)",
+				"MATCH (a:Package)-[:CONTAINS]->(b:Package) RETURN id(a), id(b)",
 				// Package -> Types (Class, Interface, Enum, Annotation)
-				"MATCH (p:Package)-[:CONTAINS]->(t:Type:Class) RETURN id(p), id(t)",
-				"MATCH (p:Package)-[:CONTAINS]->(t:Type:Interface) RETURN id(p), id(t)",
-				"MATCH (p:Package)-[:CONTAINS]->(t:Type:Enum) RETURN id(p), id(t)",
-				"MATCH (p:Package)-[:CONTAINS]->(t:Type:Annotation) RETURN id(p), id(t)"
+				"MATCH (a:Package)-[:CONTAINS]->(b:Type:Class) RETURN id(a), id(b)",
+				"MATCH (a:Package)-[:CONTAINS]->(b:Type:Interface) RETURN id(a), id(b)",
+				"MATCH (a:Package)-[:CONTAINS]->(b:Type:Enum) RETURN id(a), id(b)",
+				"MATCH (a:Package)-[:CONTAINS]->(b:Type:Annotation) RETURN id(a), id(b)"
 		};
 	}
 }
